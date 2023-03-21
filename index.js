1f18279fca688c968c0b89b58d01d3e61c9bd48e
damian kohut <damiankohut@gmai.com>
2023-03-21 16:49:33 -0400

    stuff

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 5002;
+
+// addData()
+// app.use(cors())
+
+//middleware
+app.use(express.json());
+// app.disable('view cache');
+
+//router
+app.get("/", (req, res) => {
+  res.send("hw");
+});
+
+app.listen(port, () => {
+  console.log("server started");
+});
